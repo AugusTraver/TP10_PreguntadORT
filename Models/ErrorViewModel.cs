9a2d9f85f6d0c23654b71376993d988c61e9c262
AUGUSTO AMADEO TRAVERSO <49315675@ort>
2025-09-26 14:16:22 -0300

    INICIO

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP10_AhorcadORT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
